dcc28ba8579f8f255d4877be17ddbc26e5746007
Luis Ramón López López <lrlopez@gmail.com>
2022-11-15 13:21:55 +0100

    Creada entidad Incidencia

---
+++ src/Entity/Incidencia.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="incidencia")
+ */
+class Incidencia
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     * @var int|null
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    private $titulo;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     * @var string|null
+     */
+    private $descripcion;
+
+    /**
+     * @ORM\Column(type="boolean")
+     * @var bool
+     */
+    private $prioritaria;
+
+    /**
+     * @ORM\Column(type="date")
+     * @var \DateTime
+     */
+    private $fechaApertura;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     * @var \DateTime|null
+     */
+    private $fechaCierre;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitulo(): ?string
+    {
+        return $this->titulo;
+    }
+
+    public function setTitulo(string $titulo): Incidencia
+    {
+        $this->titulo = $titulo;
+        return $this;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion(?string $descripcion): Incidencia
+    {
+        $this->descripcion = $descripcion;
+        return $this;
+    }
+
+    public function isPrioritaria(): ?bool
+    {
+        return $this->prioritaria;
+    }
+
+    public function setPrioritaria(bool $prioritaria): Incidencia
+    {
+        $this->prioritaria = $prioritaria;
+        return $this;
+    }
+
+    public function getFechaApertura(): ?\DateTime
+    {
+        return $this->fechaApertura;
+    }
+
+    public function setFechaApertura(\DateTime $fechaApertura): Incidencia
+    {
+        $this->fechaApertura = $fechaApertura;
+        return $this;
+    }
+
+    public function getFechaCierre(): ?\DateTime
+    {
+        return $this->fechaCierre;
+    }
+
+    public function setFechaCierre(?\DateTime $fechaCierre): Incidencia
+    {
+        $this->fechaCierre = $fechaCierre;
+        return $this;
+    }
+}
